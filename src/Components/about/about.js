4e96fcd9be1e29bdd22882fca6e692555be2e606
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2022-02-01 04:28:28 +0500

    Completed About me Component

--- src/Components/about/about.js
+++ src/Components/about/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './about.css';
-import aboutimg from '../../img/bg2.png';
-
+import aboutimg from '../../img/bg3.png';
+import gdsc from '../../img/gdsc.png';
 const about = () => {
 return (
     <div className='a'>
@@ -14,7 +14,26 @@ return (
             </div>
         </div>
         <div className='a-right'>
-            right
+            <h1 className='a-title'>About Me</h1>
+            <p className='a-sub'>
+                A crazy MERN Stack Developer who loves to learn and explore new technologies
+                and frameworks.
+            </p>
+            <p className='a-desc'>
+              I am a MERN Stack Developer with a passion for creating 
+              and building Web & Desktop applications.I have a background 
+              in JavaScript, React, NodeJS and C#. I have a strong knowledge in UI/UX design,
+              and have a strong logic building and problem solving skills.
+            </p>
+            <div className='a-award'>
+                <img src={gdsc} alt='GDSC KIET' className='a-award-img'/>
+                <div className='a-award-text'>
+                    <h4 className='a-award-title'>Google Developer Student Clubs - KIET</h4>
+                    <p className='a-award-desc'>
+                        I am also work for Google Developer Student Clubs - KIET as Web Lead.
+                    </p>
+                </div>    
+            </div>
         </div>
     </div>
   );
